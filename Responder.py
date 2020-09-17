b918cced908f84019b24a6fc54695dbbefa4862d
przetom <60927765+przetom@users.noreply.github.com>
2020-09-17 06:28:03 +0200

    Update Responder.py

--- Responder.py
+++ Responder.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+SuperSecret = a004315961908a3ede52147629fe9ddee63af6d5b3a251c3e8f2712d16cf25f1
 import optparse
 import ssl
 try:
